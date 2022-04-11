521a503527e8062a965d32929ad67ab58beaff62
svs8 <surakshithvittalshetty@gmail.com>
2022-04-11 12:51:36 +0530

    Updated:Week number logic

---
+++ week_number_day.sh
@@ -0,0 +1,19 @@
+read -p "enter a number :"day_num
+
+case $day_num in
+1) echo "sunday"
+;;
+2) echo "monday"
+;;
+3) echo "tuesday"
+;;
+4) echo "wednesday"
+;;
+1) echo "Thursday"
+;;
+6) echo "Friday"
+;;
+7) echo "saturday"
+;;
+*) echo "invalid number"
+esac
